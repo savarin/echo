4220aae17f0c122e948ae924ea5822ccbf0149fa
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-08-18 02:58:09 -0700

    introduce EchoServiceRpc

--- src/test/EchoServiceTest.kt
+++ src/test/EchoServiceTest.kt
@@ -1,9 +1,15 @@
+package echo.service
+
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class EchoServiceTest {
     @Test
     fun `echo should return the same message`() {
-        assertEquals("Hello, World!", EchoService().echo("Hello, World!"))
+        val echoService = EchoService()
+        val message = "Hello, World!"
+        val result = echoService.echo("x")
+
+        assertEquals(message, result)
     }
 }
