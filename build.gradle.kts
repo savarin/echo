e528e121de584bf00f09f21c82a48d9b45dde8d2
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-08-19 03:16:26 -0700

    introduce echo-service.proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.google.protobuf.gradle.*
+
 plugins {
+    id("com.google.protobuf") version "0.9.4"
     kotlin("jvm") version "1.8.22"
 }
 
@@ -7,8 +10,30 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+    implementation("io.grpc:grpc-protobuf:1.57.2")
+    implementation("io.grpc:grpc-stub:1.57.2")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.24.0"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.57.2"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+            }
+        }
+    }
 }
 
 tasks.test {
