df4fb5c999273f79cb8bbc1cc4c5564412b9eca5
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-08-19 03:12:31 -0700

    rewrite EchoService with standard directory format and more comments

--- src/test/kotlin/com/echo/service/EchoServiceTest.kt
+++ src/test/kotlin/com/echo/service/EchoServiceTest.kt
@@ -1,4 +1,4 @@
-package echo.service
+package com.echo.service
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.Test
 class EchoServiceTest {
     @Test
     fun `echo should return the same message`() {
+        // Initialize EchoService
         val echoService = EchoService()
+
+        // Define the expected message and invoke the echo method with a different input
         val message = "Hello, World!"
         val result = echoService.echo(message)
 
+        // Verify that the result is equal to the expected message
         assertEquals(message, result)
     }
 }
