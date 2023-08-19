3a7a363efc36ebe678497768f3dd083c98a9a439
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-08-19 04:28:00 -0700

    reintroduce EchoServiceRpc

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation("io.grpc:grpc-core:1.57.2")
     implementation("io.grpc:grpc-kotlin-stub:1.3.0")
     implementation("io.grpc:grpc-protobuf:1.57.2")
     implementation("io.grpc:grpc-stub:1.57.2")
+    implementation("io.grpc:grpc-testing:1.57.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -26,11 +29,15 @@ protobuf {
         id("grpc") {
             artifact = "io.grpc:protoc-gen-grpc-java:1.57.2"
         }
+        id("grpc-kotlin") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
+        }
     }
     generateProtoTasks {
         all().forEach { task ->
             task.plugins {
                 id("grpc")
+                id("grpc-kotlin")
             }
         }
     }
---
+++ src/test/kotlin/com/echo/rpc/EchoServiceRpcTest.kt
@@ -0,0 +1,52 @@
+package com.echo.rpc
+
+import com.echo.proto.EchoRequest
+import com.echo.proto.EchoResponse
+import com.echo.proto.EchoServiceGrpcKt
+import com.echo.service.EchoService
+import io.grpc.inprocess.InProcessServerBuilder
+import io.grpc.inprocess.InProcessChannelBuilder
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class EchoServiceRpcTest {
+
+    private lateinit var serverName: String
+    private lateinit var server: io.grpc.Server
+    private lateinit var channel: io.grpc.ManagedChannel
+
+    @BeforeEach
+    fun setup() {
+        // Generate unique name for in-process server
+        serverName = InProcessServerBuilder.generateName()
+
+        // Create and start an in-process server with EchoServiceRpc implementation
+        server = InProcessServerBuilder.forName(serverName).directExecutor()
+            .addService(EchoServiceRpc(EchoService())).build().start()
+
+        // Create in-process channel for communication with the server
+        channel = InProcessChannelBuilder.forName(serverName).directExecutor().build() as io.grpc.ManagedChannel
+    }
+
+    @AfterEach
+    fun cleanup() {
+        channel.shutdown()
+        server.shutdownNow()
+    }
+
+    @Test
+    fun `test echo rpc`() = runBlocking {
+        // Define request message
+        val requestMessage = "Hello, World!"
+        val request = EchoRequest.newBuilder().setMessage(requestMessage).build()
+
+        // Invoke RPC call and get the response
+        val response = EchoServiceGrpcKt.EchoServiceCoroutineStub(channel).echo(request)
+
+        // Verify that the response is as expected
+        assertEquals(EchoResponse.newBuilder().setMessage(requestMessage).build(), response)
+    }
+}
