c1038d4df39a8115eeb486454d5852e536dc6931
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-08-20 11:04:56 -0700

    introduce EchoLogStore and start with working storage

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,14 @@ repositories {
 dependencies {
     implementation("io.grpc:grpc-core:1.57.2")
     implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+    implementation("io.grpc:grpc-netty:1.41.0")
     implementation("io.grpc:grpc-protobuf:1.57.2")
     implementation("io.grpc:grpc-stub:1.57.2")
     implementation("io.grpc:grpc-testing:1.57.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("io.grpc:grpc-netty:1.41.0")
+    implementation("org.jooq:jooq:3.18.6")
+    implementation("org.xerial:sqlite-jdbc:3.42.0.0")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -60,4 +63,9 @@ val runClient by tasks.registering(JavaExec::class) {
     mainClass.set("com.echo.client.EchoClientKt")
     classpath = mainSourceSet.runtimeClasspath
     args = listOf(project.properties["args"]?.toString() ?: "")
+}
+
+val runDb by tasks.registering(JavaExec::class) {
+    mainClass.set("com.echo.store.EchoLogStoreKt")
+    classpath = mainSourceSet.runtimeClasspath
 }
---
+++ src/main/kotlin/com/echo/store/EchoLogStore.kt
@@ -0,0 +1,49 @@
+package com.echo.store
+
+import org.jooq.DSLContext
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import org.jooq.impl.SQLDataType
+import java.sql.DriverManager
+
+object EchoLogStore {
+    private const val URL = "jdbc:sqlite:echo.db"
+
+    private fun connect(): DSLContext {
+        return DSL.using(DriverManager.getConnection(URL), SQLDialect.SQLITE)
+    }
+
+    fun createTable() {
+        val ctx = connect()
+        ctx.createTableIfNotExists("echo_log")
+            .column("id", SQLDataType.VARCHAR(255).nullable(false))
+            .column("message", SQLDataType.VARCHAR(255).nullable(false))
+            .column("created_at", SQLDataType.TIMESTAMP.nullable(false))
+            .constraint(DSL.primaryKey("id"))
+            .execute()
+    }
+
+    fun insertLog(id: String, message: String, createdAt: String) {
+        val ctx = connect()
+        ctx.insertInto(DSL.table("echo_log"))
+            .columns(DSL.field("id"), DSL.field("message"), DSL.field("created_at"))
+            .values(id, message, createdAt)
+            .execute()
+    }
+
+    fun printLogs() {
+        val ctx = connect()
+        ctx.select()
+            .from("echo_log")
+            .fetch()
+            .forEach { record ->
+                println("ID: ${record["id"]}, Message: ${record["message"]}, Created At: ${record["created_at"]}")
+            }
+    }
+}
+
+fun main() {
+    EchoLogStore.createTable()
+    EchoLogStore.insertLog("id1", "Hello, World!", "2023-08-18 10:30:00")
+    EchoLogStore.printLogs()
+}
